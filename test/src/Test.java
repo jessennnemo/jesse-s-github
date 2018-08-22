11b8e9886c0a50a6fc2db1f90c8b608178adf072
jesse <jesse_er1995@outlook.com>
2018-08-22 11:39:37 +0800

    5th time commit project in idea

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         Service service=new Service();
-        System.out.println(service.count(798));
+        System.out.println(service.count(798795));
     }
 }
